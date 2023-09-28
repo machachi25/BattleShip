a228b1e336922a23dab798c2d9af33ac63c69449
Chinmay <machachi25@7hills.org>
2023-09-28 13:06:00 -0400

    Creating the input of the for users to type coor.

--- Battleship.java
+++ Battleship.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class Battleship
 {
+    Scanner ship = new Scanner(System.in);
     int numRows = 10;
     int numCols = 10;
     int[] ships = new  int[5];
 
     int[][] game = new int[numRows][numCols];
 
+    
+    public void shipPlace()
+    {
+        int xPos;
+        int yPos;
+    }
+    
     public void printBoard()
     {
         for(int r = 0; r < 10; r++ )
