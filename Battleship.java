90f777f530e71f93da572ef938bd2ac924a82cf5
Chinmay <machachi25@7hills.org>
2023-09-28 12:55:34 -0400

    starting hw: Place the ships

--- Battleship.java
+++ Battleship.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Battleship
 {
     int numRows = 10;
--- Tester.java
+++ Tester.java
@@ -2,6 +2,8 @@ public class Tester {
     public static void main(String[] args)
     {
         Battleship b1 = new Battleship();
+        b1.game[1][6] = 8;
         b1.printBoard();
+        System.out.println("");
     }
 }
