079fc38b945ea2ba645b21ed1cfea6d2642a35e4
Chinmay <machachi25@7hills.org>
2023-09-28 12:32:59 -0400

    Created the array board comprised of 0

---
+++ Battleship.java
@@ -0,0 +1,20 @@
+public class Battleship
+{
+    int numRows = 10;
+    int numCols = 10;
+    int[] ships = new  int[5];
+
+    int[][] game = new int[numRows][numCols];
+
+    public void printBoard()
+    {
+        for(int r = 0; r < 10; r++ )
+        {
+            for (int c = 0; c < 10; c++ )
+            {
+                System.out.print(game[r][c] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args)
+    {
+        Battleship b1 = new Battleship();
+        b1.printBoard();
+    }
+}
