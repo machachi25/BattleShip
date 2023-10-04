9c824d0c47bd46d2d4d89474f5805dcfecffe61d
Chinmay <machachi25@7hills.org>
2023-10-04 12:33:43 -0400

    adding more instructions print statements

--- Battleship.java
+++ Battleship.java
@@ -12,7 +12,7 @@ public class Battleship
      int yPos;
      int direction;
      String type;
-     final int Destroyer = 2;
+     final int destroyer = 2;
      final int carrier = 5;
      final int cruiser = 3;
      final int submarine = 3;
@@ -26,8 +26,9 @@ public class Battleship
         System.out.println("The board is a 10 x 10 square");
         System.out.println(" 1 is north, 2 is west, 3 is south, 4 is east");
         System.out.println("What ship would you like to place?");
+        System.out.println("The ships : ")
         type = ship.nextLine();
-        System.out.println("What row would you like your" + type +  "to go in? ");
+        System.out.println("What row would you like your " + type +  " to go in? ");
         xPos = ship.nextInt();
         System.out.println("What column would you like to place your ship? ");
         yPos = ship.nextInt();
