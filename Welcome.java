94e3752ba5a5c26afe7f64443592c11139ef2a54
Chinmay Machavaram <machachi25@7hills.org>
2023-10-10 19:20:36 -0400

    The battleship class is not running through one of the methods, so Im trying to figure that out

--- Welcome.java
+++ Welcome.java
@@ -3,9 +3,15 @@ public class Welcome {
     public static void main(String[] args)
     {
         Scanner mode = new Scanner(System.in);
+        String str; //want to add conditional that says if user says "Fast" or "fast", then send a call to the fast class but Im't not sure how to do that
         System.out.println("Hello There! Welcome to battleship!");
         System.out.println("Would you rather play the fast mode or the Regular mode?");
-        String str; //want to add conditional that says if user says "Fast" or "fast", then send a call to the fast class but Im't not sure how to do that
+        str = mode.nextLine();
+        if(str == "fast" || str == "Fast")
+        {
+            
+        }
+       
         
     }
 }
