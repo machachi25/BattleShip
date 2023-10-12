829da385e221b9613c9e48b5c4f5ad5c09393c73
Chinmay Machavaram <machachi25@7hills.org>
2023-10-11 22:17:22 -0400

    chaning welcome to boolean so that when user inputs fast/regular,  a true false value should return

--- Tester.java
+++ Tester.java
@@ -4,23 +4,12 @@ public class Tester {
         Fast f1 = new Fast();//instance of the fast class
         Battleship b1 = new Battleship(); // want to add all my class into the tester; instance of battleship class
         Welcome w1 = new Welcome();//instance of the welcome class
-        //w1.welcome();
+        String Str;
+        Str = w1.welcome();
         f1.welcome(); 
         b1.welcome();
         b1.printBoard();
         b1.shipPlace();
-        //do
-        {
-            
-        }
-        //if(Str.equalsIgnoreCase("fast"))
-        {
-            
-        }
-       //else if(Str.equalsIgnoreCase("regular"))
-       {
-        //call Battleship.java
-       }
         //b1.setDirection();
     }
 }
--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // whole point of this "Welcome" class is to act as a conditional that calls to the other classes
 public class Welcome {
-    public String welcome()
+    public boolean welcome()
     {
         Scanner mode = new Scanner(System.in);
         String Str; //want to add conditional that says if user says "Fast" or "fast", then send a call to the fast class but I'm not sure how to do that
@@ -8,6 +8,13 @@ public class Welcome {
         System.out.println("Would you rather play the fast mode or the Regular mode?");
         Str = mode.nextLine();
         mode.close();
-        return Str;
+        if(Str.equalsIgnoreCase("fast"))
+        {
+            
+        }
+       else if(Str.equalsIgnoreCase("regular"))
+       {
+        //call Battleship.java
+       }
     }
 }
