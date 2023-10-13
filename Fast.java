880e1407a029325d4ceb632e4840c53b657c9a42
Chinmay <machachi25@7hills.org>
2023-10-13 15:54:20 -0400

    trying to make a random int for the place of the ships in the fast class

--- Fast.java
+++ Fast.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
-//fast class is the class th
+import java.util.Scanner;//fast class is the class for the fast mode
+//import java.util.Random; //random ints for the place of the ships
 public class Fast
  {
     Scanner battle = new Scanner(System.in);
+    double order = (Math.random() * 100);
     int rows = 8; //similar to regular, board wise
     int columns =  8;
     int[] pieces = new int[3];
 
     int[][] speed = new int[rows][columns];
-    int x;
-    int y;
     int location;
     String species;
     final int destroyer = 2;
@@ -21,6 +20,20 @@ public class Fast
         System.out.println("The board is an 8x8");
         System.out.println("1 is north, 2 is west, 3 is south, 4 is east");
         System.out.println("Ships: Destroyer(2), Cruiser(3), Submarine(3)"); // want to add a varibale that uses a random int value, and that int will place the ship anywhere on the 2d array
+        if(order > 100)
+        {
+            for(int x =0; x < 8; x++)
+            {
+             System.out.println("1");
+            }
+        }
+        else
+        {
+            for(int y = 0; y < 8; y++)
+            {
+                System.out.println("1");
+            }
+        }
     }
 
     public void welcome()
