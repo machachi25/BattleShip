3b1a2ae72849ed9a9dc6b7de83fc1098aff566d6
Chinmay Machavaram <machachi25@7hills.org>
2023-10-19 05:31:11 -0400

    finla commit before submission

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ public class Tester {
     
     public static void main(String[] args)
     {
-        int numRows = 10;
+        int numRows = 10; // redeclaration of variables from the battleship class from the validator
         int numCols = 10;
         int[][] game = new int[numRows][numCols];
         int xPos = 0;
@@ -12,18 +12,18 @@ public class Tester {
         Welcome w1 = new Welcome();//instance of the welcome class
         Fast f1 = new Fast();//instance of the fast class
         Battleship b1 = new Battleship(); // want to add all my class into the tester; instance of battleship class
-        String str = "";
+       //String str = ""; unused variable
         //Str = w1.welcome();
        if(w1.welcome())
         { 
             f1.welcome();  //direct to the fast class
-            f1.placeShip();
-            f1.gameBoard();
+            //f1.placeShip();
+            f1.gameBoard();// prints fast board
         }
         else
         {
-            b1.welcome();
-            b1.printBoard();
+            b1.welcome();// call from the welcome class
+            b1.printBoard();// direct to place ship class
             b1.shipPlace();  //direct to battleship class
             b1.validator(game, xPos, yPos, direction, length);
         }
